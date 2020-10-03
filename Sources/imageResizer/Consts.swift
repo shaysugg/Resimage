0470efee18cbe768625b926f3564359193105c8a
Shayan <metrosug@gmail.com>
2020-10-03 22:20:00 +0330

    Add Compress-Multiple command

---
+++ Sources/imageResizer/Consts.swift
@@ -0,0 +1,42 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sha Yan on 10/1/20.
+//
+
+import Foundation
+
+enum ResizeError: Error {
+    case unvalidURL
+    case unvalidStoreToPath
+    case unvalidImageData
+    case unvalidImageFormat
+    case unvalidGivenSize
+    case bufferingError
+    
+}
+
+enum CompressError: Error {
+    case unvalidCompressSize
+    case unvalidGivenURL
+    case unvalidImageData
+    case writingCompressedImageData
+}
+
+
+enum ImageFormat: String {
+    case jpg = "jpg"
+    case png = "png"
+    
+    var pathString: String {
+        return self.rawValue
+    }
+    
+    var cfsting: CFString {
+        switch self {
+        case .jpg: return kUTTypeJPEG
+        case .png: return kUTTypePNG
+        }
+    }
+}
